205bbaf3af4f2ce443851c86d1dd756e3f77d16d
Oliver Schafeld <oliver@schafeld.com>
2025-11-04 13:26:46 +0100

    make part of the plugin text editable via extensions dashboard

--- custom/plugins/MyFirstPlugin/src/Storefront/Controller/HelloWorldController.php
+++ custom/plugins/MyFirstPlugin/src/Storefront/Controller/HelloWorldController.php
@@ -5,6 +5,7 @@ namespace OllisPlugin\Storefront\Controller;
 use OllisPlugin\Service\ProductService;
 use Shopware\Core\PlatformRequest;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Storefront\Controller\StorefrontController;
 use Shopware\Storefront\Framework\Routing\StorefrontRouteScope;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,10 +16,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class HelloWorldController extends StorefrontController
 {
     private ProductService $productService;
+    private SystemConfigService $systemConfigService;
 
-    public function __construct(ProductService $productService)
-    {
+    public function __construct(
+        ProductService $productService,
+        SystemConfigService $systemConfigService
+    ) {
         $this->productService = $productService;
+        $this->systemConfigService = $systemConfigService;
     }
 
     #[Route(
@@ -31,10 +36,17 @@ class HelloWorldController extends StorefrontController
         // Get some products to display
         $products = $this->productService->getActiveProducts($context->getContext(), 5);
         
+        // Get the configurable text from system configuration
+        $configText = $this->systemConfigService->get(
+            'MyFirstPlugin.config.textField',
+            $context->getSalesChannel()->getId()
+        );
+        
         return $this->renderStorefront('@MyFirstPlugin/storefront/page/hello-world.html.twig', [
             'message' => 'Hello World from Olli\'s Plugin!',
             'products' => $products,
-            'totalProducts' => count($products)
+            'totalProducts' => count($products),
+            'configText' => $configText ?? 'Default text from plugin'
         ]);
     }
 }
